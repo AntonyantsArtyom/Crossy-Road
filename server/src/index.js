d2629b9d79155654fc7a2a3c96abd6840a2850d7
Артем Антонянц <aa.artem.0303@mail.ru>
2025-07-15 20:04:53 +0500

    feat: make server watch mode

---
+++ server/src/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ msg: "World" });
+});
+
+app.listen(3000, () => {
+  console.log("Server is listening at http://localhost:3000");
+});
